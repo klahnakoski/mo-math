d3d2374e820aa8d3666f26e20e9fd77d38464e69
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-01-29 12:40:39 -0500

    updates from other projects

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_packages(where='.', lib_prefix='', exclude=()):
 
 setup(
     name='mo-math',
-    version="1.0.17227",
+    version="1.2.17227",
     description='More Math! Many of the aggregates you are familiar with, but they ignore Nones',
     long_description=long_desc,
     author='Kyle Lahnakoski',
