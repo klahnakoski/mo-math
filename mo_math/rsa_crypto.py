9cc325cc23cde896db5ce914071aabc673e2f470
Kyle Lahnakoski <kyle@lahnakoski.com>
2019-11-14 17:37:00 -0500

    comments

--- mo_math/rsa_crypto.py
+++ mo_math/rsa_crypto.py
@@ -1,3 +1,13 @@
+# encoding: utf-8
+#
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from __future__ import absolute_import, division, unicode_literals
+
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
